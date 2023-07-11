024b83762e0340678a5a149ec0642700119fb319
Maria Celeste Antelo <103297382+celes00@users.noreply.github.com>
2023-07-11 10:03:21 -0300

    seguimos probando con test, en este caso if else, ramificaciones

--- ElementosBasicosPython/caja_negra.py
+++ ElementosBasicosPython/caja_negra.py
@@ -10,6 +10,13 @@ class CajaNegraTest (unittest.TestCase):
         
         resultado = suma(num_1, num_2)
         self.assertEqual(resultado, 15)
+    
+    def test_suma_dos_negativos(self):
+        num_1 = -10
+        num_2 = -7
+        
+        resultado = suma(num_1, num_2)
+        self.assertEqual(resultado, -17)
         
 if __name__ == "__main__":
     unittest.main()
