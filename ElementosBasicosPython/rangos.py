2044e0306032ce0e7d492f8e237f82553e0b2ea4
Maria Celeste Antelo <103297382+celes00@users.noreply.github.com>
2023-07-11 09:00:08 -0300

    Estamos implementando un test unitario, la idea es chequear resultados posibles

---
+++ ElementosBasicosPython/rangos.py
@@ -0,0 +1,8 @@
+"""Generamos una loop para que me de el rango de nueros pares del 0 al 100, entonces utilizando la funcion range, estabezco el inicio el fin y los pasos. REcordar que el finalno se incluye, por eso 101"""
+"""for i in range(0,101,2):
+    print(i)"""
+    
+"""ahora generar un rango para que me den los numeros impares""" 
+for i in range(1,100,2):
+    print(i)
+    
